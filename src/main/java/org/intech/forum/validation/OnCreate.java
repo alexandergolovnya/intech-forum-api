0494326492007d1fc162d9339bedcc41b92892c1
Alexander Golovnya <aleksandergolovnya@gmail.com>
2019-09-17 18:20:00 +0300

    Add marker interfaces for validation rules.
    Interfaces will be used for grouping validation rules for different operations.

---
+++ src/main/java/org/intech/forum/validation/OnCreate.java
@@ -0,0 +1,12 @@
+package org.intech.forum.validation;
+
+/**
+ * Marker interface for validation.
+ * Indicates that validation rule should be used only while
+ * creation of an object.
+ *
+ * @author: Alexander Golovnya <mail@alexandergolovnya.ru>
+ * @created: 2019/09/17
+ */
+public interface OnCreate {
+}
