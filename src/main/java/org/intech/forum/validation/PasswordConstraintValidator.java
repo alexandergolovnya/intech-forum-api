cac770c288dba2f6161c748ae5332ec5549e9174
Alexander Golovnya <aleksandergolovnya@gmail.com>
2019-09-17 13:14:37 +0300

    Add password validation for account.

---
+++ src/main/java/org/intech/forum/validation/PasswordConstraintValidator.java
@@ -0,0 +1,39 @@
+package org.intech.forum.validation;
+
+import org.passay.*;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author: Alexander Golovnya <mail@alexandergolovnya.ru>
+ * @created: 2019/09/17
+ */
+public class PasswordConstraintValidator implements ConstraintValidator<Password, String> {
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+                new LengthRule(8, 60),
+                new CharacterRule(EnglishCharacterData.UpperCase, 1),
+                new CharacterRule(EnglishCharacterData.LowerCase, 1),
+                new CharacterRule(EnglishCharacterData.Digit, 1),
+                new CharacterRule(EnglishCharacterData.Special, 1),
+                new WhitespaceRule()));
+
+        RuleResult result = validator.validate(new PasswordData(password));
+        if (result.isValid()) {
+            return true;
+        }
+        List<String> messages = validator.getMessages(result);
+
+        String messageTemplate = String.join(" ", messages);
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(messageTemplate)
+                .addConstraintViolation()
+                .disableDefaultConstraintViolation();
+        return false;
+    }
+}
