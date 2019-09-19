6c8d7e5e0a2b29182e105228c8291a0ac46a87a4
Alexander Golovnya <aleksandergolovnya@gmail.com>
2019-09-19 14:12:24 +0300

    Improve topic and topic message dto classes.
    Add new fields and new validation rules.

--- src/main/java/org/intech/forum/domain/dto/TopicDto.java
+++ src/main/java/org/intech/forum/domain/dto/TopicDto.java
@@ -1,8 +1,13 @@
 package org.intech.forum.domain.dto;
 
 import lombok.Data;
+import org.intech.forum.validation.OnCreate;
+import org.intech.forum.validation.OnUpdate;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import java.time.LocalDateTime;
 
 /**
  * @author: Alexander Golovnya <mail@alexandergolovnya.ru>
@@ -11,10 +16,18 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class TopicDto {
 
-    private int id;
+    @Null(groups = OnCreate.class, message = "On topic creating id should be null")
+    @NotNull(groups = OnUpdate.class, message = "On topic editing id couldn't be null")
+    private Integer id;
 
     @NotBlank(message = "Topic title may not be blank")
     private String title;
 
+    @NotNull(groups = OnUpdate.class, message = "On topic editing description couldn't be null")
     private String description;
+
+    @Null(groups = OnCreate.class,
+            message = "Topic filed 'lastMessageDateTime' couldn't be assigned directly on creation, it should be null")
+    @Null(groups = OnUpdate.class, message = "Topic filed 'lastMessageDateTime' couldn't be null on editing")
+    private LocalDateTime lastMessageDateTime;
 }
--- src/main/java/org/intech/forum/domain/dto/TopicMessageDto.java
+++ src/main/java/org/intech/forum/domain/dto/TopicMessageDto.java
@@ -1,10 +1,13 @@
 package org.intech.forum.domain.dto;
 
 import lombok.Data;
-import org.intech.forum.domain.entity.Account;
+import org.intech.forum.validation.OnCreate;
+import org.intech.forum.validation.OnUpdate;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import java.time.LocalDateTime;
 
 /**
  * @author: Alexander Golovnya <mail@alexandergolovnya.ru>
@@ -13,7 +16,9 @@ import javax.validation.constraints.NotNull;
 @Data
 public class TopicMessageDto {
 
-    private int id;
+    @Null(groups = OnCreate.class, message = "On topic message creating id should be null")
+    @NotNull(groups = OnUpdate.class, message = "On topic message editing id couldn't be null")
+    private Integer id;
 
     @NotBlank(message = "Topic message title may not be blank")
     private String messageTitle;
@@ -21,6 +26,14 @@ public class TopicMessageDto {
     @NotBlank(message = "Topic message body may not be blank")
     private String messageBody;
 
-    @NotNull(message = "Topic account may not be null")
-    private Account account;
+    @NotNull(message = "Topic message field 'account id' may not be null")
+    private Integer accountId;
+
+    @NotNull(message = "Topic message field 'topic id' may not be null")
+    private Integer topicId;
+
+    private LocalDateTime createDate;
+
+    @Null(message = "Topic message filed 'updateDate' couldn't be assigned directly, it should be null")
+    private LocalDateTime updateDate;
 }
